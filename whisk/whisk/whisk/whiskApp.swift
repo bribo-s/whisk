85e2f506cb5ae2904162481186159c8dde7a6e75
Brianna Shen <90362603+bribo-s@users.noreply.github.com>
2024-07-30 20:00:21 -0700

    Add files via upload

---
+++ whisk/whisk/whisk/whiskApp.swift
@@ -0,0 +1,17 @@
+//
+//  whiskApp.swift
+//  whisk
+//
+//  Created by Brianna Shen on 2024-07-30.
+//
+
+import SwiftUI
+
+@main
+struct whiskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
